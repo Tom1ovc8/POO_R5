05326440cf2324df8eb02ee0730cf9ebf7edd5b3
Tom1ovc8 <tovalle@unal.edu.co>
2025-06-29 20:42:17 -0500

    Add files via upload

---
+++ Package_2/main.py
@@ -0,0 +1,22 @@
+from Shapes.Figura import Coordenada
+from Shapes.Triangulo import Triangulo
+from Shapes.Cuadrado import Rectangulo
+
+def main():
+
+    point1 = Coordenada(0, 0)
+    point2 = Coordenada(1, 0)
+    point3 = Coordenada(0, 2)
+    triangle = Triangulo(point1, point2, point3)
+    print("El area del triángulo es:", triangle.area())
+    print("El perímetro del triángulo es:", triangle.perimetro())
+
+    alto = Coordenada(0, 0)
+    ancho = Coordenada(2, 2)
+    centro = Coordenada(1, 1)
+    rectangle = Rectangulo(ancho, alto, centro)
+    print("El area del rectangulo es:", rectangle.area())
+    print("El perímetro del rectangulo es:", rectangle.perimetro())
+
+if __name__ == "__main__":
+    main() 
